8004e47449a28afadc834492f463718863eba5b0
D7702_10 <d7702_10@d7702-10ui-imac.local>
2019-04-03 16:05:54 +0900

    Stop Watch 01

--- Stop Watch 01/ViewController.swift
+++ Stop Watch 01/ViewController.swift
@@ -13,17 +13,22 @@ class ViewController: UIViewController {
     var count = 0
     var myTimer = Timer()
     
+    @IBOutlet weak var startButton: UIBarButtonItem!
+    @IBOutlet weak var pauseButton: UIBarButtonItem!
+    @IBOutlet weak var stopButton: UIBarButtonItem!
     
     @IBOutlet weak var timeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        changeButtonState(Start: true, Pause: false, Stop: false)
         timeLabel.text = String(count)
     }
-    
 
     @IBAction func startButtonPressed(_ sender: Any) {
+        
+        changeButtonState(Start: false, Pause: true, Stop: true)
+        
         if (!myTimer.isValid) {
             myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
             print("start Button = \(myTimer.isValid)")
@@ -32,11 +37,17 @@ class ViewController: UIViewController {
     
 
     @IBAction func pauseButtonPressed(_ sender: Any) {
+        
+        changeButtonState(Start: true, Pause: false, Stop: true)
+        
         myTimer.invalidate()
     }
     
 
     @IBAction func stopButtonPressed(_ sender: Any) {
+        
+        changeButtonState(Start: true, Pause: false, Stop: false)
+        
         myTimer.invalidate()
         print(myTimer.isValid)
         
@@ -48,6 +59,14 @@ class ViewController: UIViewController {
         count += 1
         timeLabel.text = String(count)
     }
+    
+    func changeButtonState(Start : Bool, Pause : Bool, Stop : Bool) {
+        startButton.isEnabled = Start
+        pauseButton.isEnabled = Pause
+        stopButton.isEnabled = Stop
+    }
+    
+    
     
 }
 
